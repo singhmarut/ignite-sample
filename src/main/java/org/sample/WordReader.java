7356060599dbdd4317b827084519aa88c8fa3f9f
marutsingh <marut.singh@delhivery.com>
2016-12-15 22:40:49 +0530

    added configuration for client mode

--- src/main/java/org/sample/WordReader.java
+++ src/main/java/org/sample/WordReader.java
@@ -3,6 +3,7 @@ package org.sample;
 import org.apache.ignite.*;
 import org.apache.ignite.stream.StreamTransformer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.data.redis.connection.RedisConnection;
@@ -36,22 +37,15 @@ public class WordReader {
     @Autowired
     IgniteSpringBean ignite;
 
-
-    // inject the actual template
-   /* @Autowired
-    private RedisTemplate<String, String> template;
-*/
-    // inject the template as ListOperations
-    // can also inject as Value, Set, ZSet, and HashOperations
-    /*@Resource(name="redisTemplate")
-    private ListOperations<String, String> listOps;
- */
-    IgniteDataStreamer<String, Long> stmr;
     ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
     IgniteAtomicLong inventory;
 
+    @Value("${ignite.node.mode.client}")
+    Boolean clientMode;
+
     @PostConstruct
     void init(){
+        ignite.getConfiguration().setClientMode(clientMode);
         IgniteCache<String, Long> stmCache = ignite.getOrCreateCache("inventory");
         inventory = ignite.atomicLong(
                 "atomicName", // Atomic long name.
@@ -63,41 +57,10 @@ public class WordReader {
             public void run() {
                 System.out.println("Incremented value: " + inventory.get());
             }
-        },0, 5, TimeUnit.SECONDS);
+        }, 0, 5, TimeUnit.SECONDS);
     }
 
     public void addData(String word){
         inventory.incrementAndGet();
     }
-
-    public void read(){
-
-
-        // Mark this cluster member as client.
-        //Ignition.setClientMode(true);
-
-        // Create a streamer to stream words into the cache.
-            // Allow data updates.
-            // Configure data transformation to count instances of the same word.
-            stmr.receiver(StreamTransformer.from((e, arg) -> {
-                // Get current count.
-                Long val = e.getValue();
-                // Increment current count by 1.
-                e.setValue(val == null ? 1L : val + 1);
-                System.out.println(e.getValue());
-                return null;
-            }));
-
-            // Stream words from "alice-in-wonderland" book.
-            /*int i = 0;
-            while (i < 2) {
-                // Read words from a text file.
-                for (final String word : "Print words using runnable".split(" "))  {
-                    if (!word.trim().isEmpty())
-                        stmr.addData(word, 1L);
-                }
-                ++i;
-            }*/
-    }
-
 }
